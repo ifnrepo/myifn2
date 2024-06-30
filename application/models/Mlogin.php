cbb12317c6ce4f1b9d6e75480c192004d1d41619
atepasep <atep.saprudin86@gmail.com>
2024-06-30 14:48:13 +0700

    Edit aktif User

--- application/models/Mlogin.php
+++ application/models/Mlogin.php
@@ -3,7 +3,7 @@ class Mlogin extends CI_Model
 {
     public function ceklogin($a, $b)
     {
-        $query = $this->db->query("Select * from user_manajemen where login = '" . $a . "' and password = '" . $b . "' and login != '' and password != '' ");
+        $query = $this->db->query("Select * from user_manajemen where login = '" . $a . "' and password = '" . $b . "' and login != '' and password != '' and aktif = 1 ");
         return $query;
     }
 }
